0b1894fabdc3d73c87ac49520fa7b7956ca07f63
kagwi <kagwiandrew@gmail.com>
2020-08-02 23:35:01 +0300

    react chat app sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router ,Route, Switch} from 'react-router-dom';
+
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import './App.css';
 
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+// const theme = createMuiTheme()
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <MuiThemeProvider>
+ <Router>
+ <Navbar/>
+      <div>
+        <Switch>
+
+            <Route exact path="/" component={HomePage}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/signup" component={Signup}/>
+        </Switch>
+      </div>
+ 
+ </Router>
+ 
+ 
+ </MuiThemeProvider>
+
   );
 }
 
---
+++ temp.jsx
@@ -0,0 +1,109 @@
+// rfcredux
+
+            /**
+            import React, { Component } from 'react'
+            import { connect } from 'react-redux'
+
+            export const Login = () => {
+                return (
+                    <div>
+                        
+                    </div>
+                )
+            }
+
+            const mapStateToProps = (state) => ({
+                
+            })
+
+            const mapDispatchToProps = {
+                
+            }
+
+            export default connect(mapStateToProps, mapDispatchToProps)(Login)
+
+            */
+
+
+// rfce
+            /**
+
+            import React from 'react'
+            
+            function Login() {
+                return (
+                    <div>
+                        
+                    </div>
+                )
+            }
+            
+            export default Login
+            */
+ 
+
+// rfcp
+            /**
+            import React from 'react'
+            import PropTypes from 'prop-types'
+            
+            function Login(props) {
+                return (
+                    <div>
+                        
+                    </div>
+                )
+            }
+            
+            Login.propTypes = {
+            
+            }
+            
+            export default Login
+            */
+
+
+// rfcreduxp
+            /**
+            import React, { Component } from 'react'
+            import PropTypes from 'prop-types'
+            import { connect } from 'react-redux'
+            
+            export const Login = () => {
+                return (
+                    <div>
+                        
+                    </div>
+                )
+            }
+            
+            Login.propTypes = {
+                prop: PropTypes
+            }
+            
+            const mapStateToProps = (state) => ({
+                
+            })
+            
+            const mapDispatchToProps = {
+                
+            }
+            
+            export default connect(mapStateToProps, mapDispatchToProps)(Login)
+            
+            */
+
+// rafce
+            /**
+            import React from 'react'
+
+            const Login = () => {
+                return (
+                    <div>
+                        
+                    </div>
+                )
+            }
+
+            export default Login
+            */
